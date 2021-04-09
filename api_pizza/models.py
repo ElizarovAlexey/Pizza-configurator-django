50b2943c6576dd79455028e4c831e1f3986f0e13
Alexei <elizarov.alex2020@gmail.com>
2021-04-09 13:23:35 +0300

    Created models and migrations

--- api_pizza/models.py
+++ api_pizza/models.py
@@ -1,3 +1,84 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    """ Размеры """
+    value = models.SmallIntegerField('Размер', default=25)
+    cost = models.SmallIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.value
+
+
+class Dough(models.Model):
+    """ Тесто """
+    type = models.CharField('Тесто', max_length=50)
+    cost = models.SmallIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.type
+
+
+class Ingredient(models.Model):
+    """ Ингредиенты """
+    value = models.CharField('Ингредиент', max_length=50)
+    cost = models.SmallIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.value
+
+
+class Pizza(models.Model):
+    """ Пиццы """
+
+    name = models.CharField('Название', max_length=50)
+    description = models.TextField('Описание', max_length=250)
+    image = models.URLField('Изображение', max_length=70)
+    dough = models.ForeignKey(Dough, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient, 'Ингредиенты')
+    cost = models.PositiveIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Drink(models.Model):
+    """ Напитки """
+
+    name = models.CharField('Название', max_length=50)
+    image = models.URLField('Изобрание', max_length=70)
+    description = models.TextField('Описание', max_length=250)
+    cost = models.PositiveIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Dessert(models.Model):
+    """ Дессерты """
+
+    name = models.CharField('Название', max_length=50)
+    image = models.URLField('Изобрание', max_length=70)
+    description = models.TextField('Описание', max_length=250)
+    cost = models.PositiveIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    """ Корзина """
+
+    name = models.CharField('Название', max_length=50)
+    image = models.URLField('Изображение', max_length=70)
+    dough = models.ForeignKey(Dough, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient, 'Ингредиенты')
+    cost = models.PositiveIntegerField('Цена', default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Cart'
